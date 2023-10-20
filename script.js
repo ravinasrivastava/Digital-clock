6cea532c5cf75f19cc76a197fa500d38894b3480
ravinasrivastava <90607740+ravinasrivastava@users.noreply.github.com>
2023-10-20 13:21:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// Calling showTime function at every second
 setInterval(showTime, 1000);
-
-// Defining showTime funcion
 function showTime() {
-    // Getting current time and date
+  
     let time = new Date();
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
     am_pm = "AM";
-
-    // Setting time for 12 Hrs format
     if (hour >= 12) {
         if (hour > 12) hour -= 12;
         am_pm = "PM";
@@ -31,8 +26,6 @@ function showTime() {
         ":" +
         sec +
         am_pm;
-
-    // Displaying the time
     document.getElementById(
         "clock"
     ).innerHTML = currentTime;
